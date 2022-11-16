4c75ae1bf4c48bb51c7b1f1f5d71f93fd91b5c04
dimok <ddk.vlz@gmail.com>
2022-11-16 15:33:15 +0700

    Make builder instead of factory

--- tests/ExpressionTest.php
+++ tests/ExpressionTest.php
@@ -3,14 +3,13 @@
 namespace Rossonero585\PhpExpression\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Rossonero585\PhpExpression\ExpressionFactory;
+use Rossonero585\PhpExpression\ExpressionBuilder;
 
 class ExpressionTest extends TestCase
 {
-
-    public function testExecute()
+    public function testExecute() : void
     {
-        $expressionFactory = new ExpressionFactory();
+        $expressionBuilder = new ExpressionBuilder();
 
         $rates = [
             "USD" => 74,
@@ -18,20 +17,40 @@ class ExpressionTest extends TestCase
             "EUR" => 86
         ];
 
-        $expressionFactory->addFunction('convert', function ($value, $currency, $partner) use($rates) {
-            $rate = $rates[$currency];
-            if ($partner == 10) $rate = 0.9 * $rate;
-            return $value / $rate;
-        });
+        $expression = $expressionBuilder
+            ->addFunction('convert', function ($value, $currency, $partner) use ($rates) {
+                $rate = $rates[$currency];
+                if ($partner == 10) $rate = 0.9 * $rate;
+                return $value / $rate;
+            })
+            ->addArguments(["cost", "currency", "partner"])
+            ->create("1.11 * convert(cost, currency, partner) + 100");
 
-        $expression = $expressionFactory->createExpression(
-            "1.11 * convert(cost, currency, partner) + 100",
-            ["cost", "currency", "partner"]
+        $this->assertEquals(
+            round(1.11 * (100 / 74) + 100, 2),
+            $expression->execute([
+                "cost" => 100,
+                "currency" => "USD",
+                "partner" => 1
+            ])
         );
 
-        $this->assertEquals(round(1.11 * (100 / 74) + 100, 2), $expression->execute([100, "USD", 1]));
-        $this->assertEquals(round(1.11 * (100 / (86 * 0.9)) + 100, 2), $expression->execute([100, "EUR", 10]));
-        $this->assertEquals(round(1.11 * (100 / (95 * 0.9)) + 100, 2), $expression->execute([100, "GBP", 10]));
-    }
+        $this->assertEquals(
+            round(1.11 * (100 / (86 * 0.9)) + 100, 2),
+            $expression->execute([
+                "cost" => 100,
+                "currency" => "EUR",
+                "partner" => 10
+            ])
+        );
 
+        $this->assertEquals(
+            round(1.11 * (100 / (95 * 0.9)) + 100, 2),
+            $expression->execute([
+                "cost" => 100,
+                "currency" => "GBP",
+                "partner" => 10
+            ])
+        );
+    }
 }
